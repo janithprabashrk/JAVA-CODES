0b6236d707059c4d77aeda673be4d900242ae880
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-22 08:09:59 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+class Animal {
+    void bark() {
+        System.out.println("Woof-Woof");
+    }
+}
+
+public class MyClass {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.bark();
+    }
+}
