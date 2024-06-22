05822bb5a38db6d2232258133228c226ac27bcdf
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-22 07:55:20 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A {
+  public void test() {
+    System.out.println("Hi");
+  }  
+
+  public static void main(String[] args) {
+    A a = new A();  // Create an instance of the class A
+    a.test();       // Call the test method on the instance
+  }
+}
