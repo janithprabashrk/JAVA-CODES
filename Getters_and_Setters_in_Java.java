0007fae715059088bba0c65c0a49431ccd65447f
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-23 09:35:20 +0530

    Create Getters_and_Setters_in_Java.java

---
+++ Getters_and_Setters_in_Java.java
@@ -0,0 +1,13 @@
+public class Getters_and_Setters_in_Java {
+  private String color;
+
+  // Getter
+  public String getColor() {
+    return color;
+  }
+
+ // Setter
+  public void setColor(String c) {
+    this.color = c;
+  }
+}
