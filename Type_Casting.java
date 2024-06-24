a4b9136dcf1d765f42f06eba221ff8e2ffe0db00
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-24 21:36:18 +0530

    Create Type_Casting.java

---
+++ Type_Casting.java
@@ -0,0 +1,14 @@
+public class Type_Casting {
+    public static void main(String[] args) {
+        double a = 42.571;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
+
+/*Type Casting
+Assigning a value of one type to a variable of another type is known as Type Casting.
+
+To cast a value to a specific type, place the type in parentheses and position it in front of the value.
+
+ */
