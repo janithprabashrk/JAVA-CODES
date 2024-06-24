7e9ee224b710477e2576b2fae95f64663125cc06
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-24 22:13:14 +0530

    Create Array_List.java

---
+++ Array_List.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Array_List {
+    public static void main(String[ ] args) {
+        ArrayList<String> colors = new ArrayList<String>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Orange");
+        colors.remove("Green");
+        
+        System.out.println(colors);
+    }
+}
